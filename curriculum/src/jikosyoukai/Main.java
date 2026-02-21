033d776e2bc797f71a89a32208cd26f77724fdc6
user <user@WIN-V5T7QTAKTPF>
2026-02-21 16:50:39 +0900

    自己紹介修正

--- curriculum/src/jikosyoukai/Main.java
+++ curriculum/src/jikosyoukai/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	
 		public static void main(String[] args) {
 
-			// 問題5：weight に 60 を入れる
+			
 			Person p1 = new Person("鈴木太郎", 20, 1.7, 60);
 
-			// 情報表示
+			
 			p1.print();
 
-			// 合計人数表示
+			
 			Person.printCount();
 		}
 	}
